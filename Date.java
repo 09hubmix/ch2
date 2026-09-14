3520b4e6a13949894cb60afbac4737c606213174
09hubmix <andrewdong906@gmail.com>
2026-09-14 13:29:31 -0700

    Add files via upload

---
+++ Date.java
@@ -0,0 +1,26 @@
+public class Date {
+	public static void main (String[] args) {
+		
+		//variable declarations 
+		String day = "Saturday"; 
+		int date = 20;
+		String month = "Septemeber ";
+		int year = 2026;
+		
+		System.out.print("American format: ");
+		System.out.print(day);
+		System.out.print(", ");
+		System.out.print(month);
+		System.out.print(date);
+		System.out.print(", ");
+		System.out.println(year);
+		System.out.print("European format: ");
+		System.out.print(day);
+		System.out.print(" ");
+		System.out.print(date);
+		System.out.print(" ");
+		System.out.print(month);
+		System.out.println(year);
+		
+	}
+}
