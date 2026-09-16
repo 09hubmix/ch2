8892997e8b1e92067c7ea0c3672c6f543869565e
09hubmix <andrewdong906@gmail.com>
2026-09-16 12:54:07 -0700

    Add files via upload

--- Time.java
+++ Time.java
@@ -5,13 +5,15 @@ public class Time {
 	String message1 = "number of seconds since midnight: ";
 	String message2 = "number of seconds remaining in the day: ";
 	String message3 = "percentage of the day that has passed: ";
-	String message4 = "elapsed time since I started working on this exercise: ";
+	String message4 = "elapsed time since I started working on this exercise in seconds: ";
 	int hour = 12;
 	int minute = 45;
 	int second = 32;
-	double wasd;
-	int seconds = hour * 3600 + minute * 60 + second;
-	
+	int hour2 = 13;
+	int minute2 = 12;
+	int second2 = 26;
+	double seconds = hour * 3600 + minute * 60 + second;
+	int seconds2 = hour2 * 3600 + minute2 * 60 + second2;
 	//Messages
 	System.out.print(message1);
 	System.out.println(seconds);
@@ -19,5 +21,7 @@ public class Time {
 	System.out.println(86400 - 45932);
 	System.out.print(message3);
 	System.out.println(seconds/86400 * 100);
+	System.out.print(message4);
+	System.out.println(seconds2);
 		}
 }
